59e70a86d89ba0ee45b27c40fdae64530cfcb40b
Mohammad Sameer <sammusmarty563@gmail.com>
2024-02-12 01:08:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -399,3 +399,97 @@ function createListitems(listName) {
     // Save the updated lists to local storage
     saveLists();
 }
+
+
+
+
+
+
+// Function to create a new list item with the given name and timestamp
+function createListitems(listName) {
+    var containerLists = document.querySelector('.list-one');
+    var newItemContainer = document.createElement('div'); // Create a new <div> element
+    newItemContainer.className = 'list-item'; // Assign the same class as existing items
+
+    // Get the current date and time
+    var dateTime = new Date();
+    var date = dateTime.toLocaleDateString();
+    var time = dateTime.toLocaleTimeString();
+
+    // Create a new div for storing the date and time
+    var timeBox = document.createElement('div');
+    timeBox.className = 'timebox';
+
+    // Create spans for date and time
+    var dateSpan = document.createElement('span');
+    dateSpan.className = 'datecreated';
+    dateSpan.textContent = '' + date;
+
+    var timeSpan = document.createElement('span');
+    timeSpan.className = 'timecreated';
+    timeSpan.textContent = '' + time;
+
+    // Append date and time spans to timeBox
+    timeBox.appendChild(dateSpan);
+    timeBox.appendChild(timeSpan);
+
+    // Create a new <li> element with the list name
+    var newListItem = document.createElement('li');
+    newListItem.textContent = listName;
+
+    // Create div for delete and important buttons
+    var delAndImpBtns = document.createElement('div');
+    delAndImpBtns.className = 'del-and-imp-btns'; // Assign class
+
+    // Create important button
+    var mrkimpBtn = document.createElement('div');
+    mrkimpBtn.className = 'mrkimp-btn';
+    mrkimpBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                <path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/>
+                            </svg>`;
+
+    // Create task overflow button
+    var taskOverBtn = document.createElement('div');
+    taskOverBtn.className = 'task-over-btn';
+    taskOverBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>`;
+
+    // Append buttons to their container
+    delAndImpBtns.appendChild(mrkimpBtn);
+    delAndImpBtns.appendChild(taskOverBtn);
+
+    // Append elements to their respective containers
+    newItemContainer.appendChild(timeBox);
+    newItemContainer.appendChild(newListItem);
+    newItemContainer.appendChild(delAndImpBtns);
+    containerLists.prepend(newItemContainer); // Add new item to the beginning of the list
+
+    // Apply styles based on theme and clicked state
+    applyStyles(newItemContainer);
+}
+
+// Function to apply styles based on theme and clicked state
+function applyStyles(item) {
+    let theme = localStorage.getItem('theme');
+    let greenColor = localStorage.getItem('greenColor');
+
+    // Change background color based on the theme
+    item.style.backgroundColor = theme;
+
+    // Check if the item is clicked and update styles accordingly
+    if (item.classList.contains('clicked')) {
+        item.style.backgroundImage = `url(completed\ stamp.png)`; // Change background image
+        item.style.backgroundSize = '70px'; // Set background size
+        item.style.backgroundRepeat = 'no-repeat'; // Set background repeat
+        item.style.backgroundPosition = 'center'; // Set background position
+        item.style.filter = 'grayscale(1)'; // Apply grayscale filter
+
+        let listItem = item.querySelector('li');
+        listItem.style.textDecoration = 'line-through 4px'; // Apply text decoration
+        listItem.style.filter = 'blur(px)'; // Apply blur filter
+    }
+}
+
+// Apply styles to existing list items
+document.querySelectorAll('.list-item').forEach(item => {
+    applyStyles(item);
+});
