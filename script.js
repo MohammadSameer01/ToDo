41d67f7b3b7e742ce8d4b0b15e508876ba376d22
Mohammad Sameer <sammusmarty563@gmail.com>
2024-02-15 19:05:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -434,3 +434,43 @@ window.addEventListener("focus", () => {
 
 
 
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the container-lists element
+var containerLists = document.querySelector(".container-lists");
+
+// Add a click event listener to the container-lists
+containerLists.addEventListener("click", function (event) {
+    // Check if the clicked element is a direct child of container-lists
+    if (event.target.parentNode === containerLists) {
+        // Show the modal
+        modal.style.display = "block";
+    }
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
+
